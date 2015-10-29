67da3e6aabf7868c649f1e84d599049848b1f8fa
raunaqrox <sahebjot94@gmail.com>
2015-10-29 18:55:47 +0530

    day and time added

--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
+set :public_folder, File.dirname(__FILE__) + '/static'
+
 get '/' do
   "Hello world!"
+end
+
+get '/day' do
+  erb :day
+end
+
+get '/time' do
+  erb :time
 end
